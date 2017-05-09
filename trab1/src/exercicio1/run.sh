c51c954d4b01236262d0f0c268043a29792fe216
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2017-05-09 00:13:56 -0300

    adding icc to run scripts

--- trab1/src/exercicio1/run.sh
+++ trab1/src/exercicio1/run.sh
@@ -1,22 +1,28 @@
 [ -e execution_time.txt ] && rm execution_time.txt
 
-otm_lvls=('-O0' '-O1' '-O2' '-O3')
-for i in "${otm_lvls[@]}"
+compilers=('g++' 'icc')
+for compiler in "${compilers}"
 do
-    otm=$i
-    g++ main.cpp -o main.out $otm
-    echo $otm >> execution_time.txt
-    matrix_sizes=(10 100 500 1000)
-    for j in "${matrix_sizes[@]}"
+    echo $compiler >> execution_time.txt
+    otm_lvls=('-O0' '-O1' '-O2' '-O3')
+    for i in "${otm_lvls[@]}"
     do
-        n=$j
-        mult_types=(1 2 3 4)
-        for k in "${mult_types[@]}"
+        otm=$i
+        echo $otm >> execution_time.txt
+        matrix_sizes=(10)
+        for j in "${matrix_sizes[@]}"
         do
-            type=$k
-            result=$(./main.out $n $type)
-            echo $n': '$result' secs' >> execution_time.txt
+            n=$j
+            mult_types=(1 2 3 4)
+            for k in "${mult_types[@]}"
+            do
+                type=$k
+                result=$(./main.out $n $type)
+                echo $n': '$result' secs' >> execution_time.txt
+            done
         done
+        echo '' >> execution_time.txt
     done
+    echo '' >> execution_time.txt
     echo '' >> execution_time.txt
 done
