6c66ebcaf8d08133ff306dc75304d979a093a538
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2017-05-12 20:53:45 -0300

    adding script jobs

--- trab1/src/exercicio2/run.sh
+++ trab1/src/exercicio2/run.sh
@@ -1,24 +1,35 @@
 [ -e execution_time.txt ] && rm execution_time.txt
 
-otm_lvls=('-O0' '-O1' '-O3')
-for i in "${otm_lvls[@]}"
+compilers=('g++' 'icc')
+for compiler in "${compilers}"
 do
-    otm=$i
-    g++ main.cpp -o main.out $otm
-    echo $otm >> execution_time.txt
-    matrix_size=1024
-    mult_types=(1 2 3 4)
-    for j in "${mult_types[@]}"
+    echo $compiler >> execution_time.txt
+    otm_lvls=('-O0' '-O1' '-O3')
+    for i in "${otm_lvls[@]}"
     do
-        type=$j
-        nbs=(2 4 8 16)
-        for k in "${nbs[@]}"
+        otm=$i
+        if [ $compiler == 'g++' ] 
+        then
+            $compiler main.cpp -o main.out $otm
+        else
+            $compiler main.cpp -o main$otm.out -qopt-report $otm
+        fi
+        echo $otm >> execution_time.txt
+        matrix_size=1024
+        mult_types=(1 2 3 4)
+        for j in "${mult_types[@]}"
         do
-            nb=$k
-            result=$(./main.out $matrix_size $type $nb)
-            echo $nb': '$result' secs' >> execution_time.txt
+            type=$j
+            nbs=(2 4 8 16)
+            for k in "${nbs[@]}"
+            do
+                nb=$k
+                result=$(./main.out $matrix_size $type $nb)
+                echo $nb': '$result' secs' >> execution_time.txt
+            done
         done
+        echo '' >> execution_time.txt
     done
     echo '' >> execution_time.txt
-    icc main.cpp -o main$otm.out -qopt-report $otm
+    echo '' >> execution_time.txt
 done
